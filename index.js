3ad8b0904a039772f23dd493105895fec82d9413
khamang <dv@hamang.net>
2023-03-09 14:23:19 +0100

    Fixed some codeissues and set head-tag required

--- index.js
+++ index.js
@@ -1,69 +1,72 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 const context = github.context;
 
-const getRepoTags = async () => {  
+const getRepoTags = async () => {
   const response = await octokit.rest.repos.listTags({
     owner: context.repo.owner,
     repo: context.repo.repo,
-  });  
+  });
   return response.data;
-}
+};
 
-function findPreviousSemver(semverString, semverStringArray) {
+const findPreviousSemver = async (semverString, semverStringArray) => {
+  confole.log("findPreviousSemver", semverString, semverStringArray);
   if (!semverStringArray.length) {
-    return null
+    return null;
   }
 
   const sortedSemvers = semverStringArray.sort();
-  var indexOfCurrent = sortedSemvers.indexOf(semverString);  
-  if (indexOfCurrent > -1 ){
-    if (indexOfCurrent > 0){
-        return sortedSemvers[--indexOfCurrent];
+  var indexOfCurrent = sortedSemvers.indexOf(semverString);
+  if (indexOfCurrent > -1) {
+    if (indexOfCurrent > 0) {
+      return sortedSemvers[--indexOfCurrent];
     }
     return sortedSemvers[indexOfCurrent];
   }
-  return null;  
+  return null;
 }
 
-(async function() {
+(async function () {
   try {
-    const token = core.getInput('token');
+    const token = core.getInput("token");
     octokit = github.getOctokit(token);
-    console.log("Initiated octokit");    
+    console.log("Initiated octokit");
 
-    const headReleaseTag = core.getInput('head-tag')
+    const headReleaseTag = core.getInput("head-tag");
     console.log("Head release tag: ", headReleaseTag);
 
     var repoTags = await getRepoTags();
-    const tags = repoTags.map(c => c.name).sort();
-    
-    const baseReleaseTag = core.getInput('release-tag') || await findPreviousSemver(headReleaseTag, tags);
-    console.log("Previous release tag: ", baseReleaseTag);    
+    const tags = repoTags.map((c) => c.name);
 
-    const continueOnError = core.getInput('continue-on-error');
-    
+    const baseReleaseTag = core.getInput("release-tag") || await findPreviousSemver(headReleaseTag, tags);
+    console.log("Previous release tag: ", baseReleaseTag);
+
+    const continueOnError = core.getInput("continue-on-error") || false;
+
     const response = await octokit.rest.repos.compareCommitsWithBasehead({
       owner: context.repo.owner,
       repo: context.repo.repo,
       basehead: `${baseReleaseTag}...${headReleaseTag}`,
     });
-    const messages = (response.data.commits.map(c => c.commit.message) || []).join('');
-    const regex = /[A-Z]{2,}-\d+/g; 
+    const messages = (
+      response.data.commits.map((c) => c.commit.message) || []
+    ).join("");
+    const regex = /[A-Z]{2,}-\d+/g;
     const issueKeys = messages.match(regex);
 
     if (!issueKeys || issueKeys.length == 0) {
       if (!continueOnError) {
-        throw new Error("No issue keys found"); 
+        throw new Error("No issue keys found");
       }
     }
-    core.setOutput('issue-keys', issueKeys.join(','));
+    core.setOutput("issue-keys", issueKeys.join(","));
   } catch (error) {
     if (!continueOnerror) {
-     core.setFailed(error.message);
+      core.setFailed(error.message);
     } else {
-     core.setOutput('issue-keys', ''); 
+      core.setOutput("issue-keys", "");
     }
   }
-})()
+})();
